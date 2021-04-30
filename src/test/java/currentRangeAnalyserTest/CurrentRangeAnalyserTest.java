babbb0a907574a2e6b0bf547114826d7da81ed29
Praveen <ravichandran.praveen@in.bosch.com>
2021-04-30 13:41:43 +0530

    readme file updated

--- src/test/java/currentRangeAnalyserTest/CurrentRangeAnalyserTest.java
+++ src/test/java/currentRangeAnalyserTest/CurrentRangeAnalyserTest.java
@@ -45,4 +45,12 @@ public class CurrentRangeAnalyserTest {
 		assertEquals(4, countOfNumbersInDifferentRanges.get("3-5"));
 		assertEquals(3, countOfNumbersInDifferentRanges.get("10-12"));
 	}
+	
+	@Test
+	void findTheCountOfNumbersInEachRangeIfOneValueIsNull() {
+		Map<String, Integer> countOfNumbersInDifferentRanges = new CurrentRangeAnalyser()
+				.findRangeInList(Arrays.asList(3, 3, 5, 4, 10, 11, null));
+		assertEquals(4, countOfNumbersInDifferentRanges.get("3-5"));
+		assertEquals(3, countOfNumbersInDifferentRanges.get("10-12"));
+	}
 }
